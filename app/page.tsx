933fc2873d0b2d87595607ef431603770a65a448
OwenLu <gg0125gg@gmail.com>
2023-10-18 16:14:53 +0800

    feat: layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,173 @@
-import Image from 'next/image'
+import * as React from 'react';
+import Image from 'next/image';
+import logo from '../public/logo.svg';
+import menu from '../public/menu.svg';
+import bike from '../public/bike.svg';
 
-export default function Home() {
+export default function MyComponent() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className=' flex w-[auto] flex-col items-center '>
+      <div className='navbar px-5 flex relative'>
+        <div className='navbar-start md:w-3/4 gap-5'>
+          <Image src={logo} alt='logo' width={65} height={65} />
+          <div className='hidden  md:flex md:shrink-0	md:gap-5 '>
+            <div className='text-lime-700 text-lg font-medium '>使用說明</div>
+            <div className='text-lime-700 text-lg font-medium '>收費方式</div>
+            <div className='text-lime-400 text-lg font-bold '>站點資訊</div>
+            <div className='text-lime-700 text-lg font-medium '>最新消息</div>
+            <div className='text-lime-700 text-lg font-medium '>活動專區</div>
+          </div>
+        </div>
+        <div className='navbar-end'>
+          <div className='dropdown dropdown-end'>
+            <label tabIndex={0} className='md:hidden'>
+              <Image src={menu} alt='menu' width={24} height={24} />
+            </label>
+            <ul
+              tabIndex={0}
+              className='menu menu-md dropdown-content mt-7 z-[1]  bg-primary w-screen flex-auto text-white'>
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li>
+                <a>Parent</a>
+                <ul className='p-2'>
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a>Item 3</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='bg-gray-200 self-stretch w-full h-px ' />
+      <div className='w-full md:flex'>
+        <div className='w-full px-10 relative '>
+          <h1 className='top-10  relative text-primary text-lg font-bold self-start	'>
+            站點資訊
+          </h1>
+          <select className='select select-bordered w-full relative mt-14	'>
+            <option disabled selected>
+              搜尋站點
+            </option>
+            <option>台北市</option>
+            <option>台北市</option>
+          </select>
+          <select className='select select-bordered w-full relative mt-5	'>
+            <option disabled selected>
+              新竹科學園區
+            </option>
+            <option className='text-black'>Han Solo</option>
+            <option>台北市</option>
+          </select>
+          <div className='form-control mt-5'>
+            <label className='label cursor-pointer flex-row justify-start gap-2'>
+              <input type='checkbox' className='checkbox checkbox-primary' />
+              <span className='label-text text-black'>全部勾選</span>
+            </label>
+            <div className='grid grid-cols-3 md:grid md:grid-cols-4'>
+              <label className='label cursor-pointer flex-row justify-start gap-2'>
+                <input type='checkbox' className='checkbox checkbox-primary' />
+                <span className='label-text text-black'>松山區</span>
+              </label>
+              <label className='label cursor-pointer flex-row justify-start gap-2'>
+                <input type='checkbox' className='checkbox checkbox-primary' />
+                <span className='label-text text-black'>松山區</span>
+              </label>
+              <label className='label cursor-pointer flex-row justify-start gap-2'>
+                <input type='checkbox' className='checkbox checkbox-primary' />
+                <span className='label-text text-black'>松山區</span>
+              </label>
+              <label className='label cursor-pointer flex-row justify-start gap-2'>
+                <input type='checkbox' className='checkbox checkbox-primary' />
+                <span className='label-text text-black'>松山區</span>
+              </label>
+              <label className='label cursor-pointer flex-row justify-start gap-2'>
+                <input type='checkbox' className='checkbox checkbox-primary' />
+                <span className='label-text text-black'>松山區</span>
+              </label>
+              <label className='label cursor-pointer flex-row justify-start gap-2'>
+                <input type='checkbox' className='checkbox checkbox-primary' />
+                <span className='label-text text-black'>松山區</span>
+              </label>
+              <label className='label cursor-pointer flex-row justify-start gap-2'>
+                <input type='checkbox' className='checkbox checkbox-primary' />
+                <span className='label-text text-black'>松山區</span>
+              </label>
+              <label className='label cursor-pointer flex-row justify-start gap-2'>
+                <input type='checkbox' className='checkbox checkbox-primary' />
+                <span className='label-text text-black'>松山區</span>
+              </label>
+              <label className='label cursor-pointer flex-row justify-start gap-2'>
+                <input type='checkbox' className='checkbox checkbox-primary' />
+                <span className='label-text text-black'>松山區</span>
+              </label>
+            </div>
+          </div>
+        </div>
+        <Image src={bike} alt='bike' className='hidden md:flex self-end	' />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className='overflow-x-auto'>
+        <table className='table table-zebra '>
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr>
+              <th>1</th>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+              <td>Blue</td>
+            </tr>
+            {/* row 2 */}
+            <tr>
+              <th>2</th>
+              <td>Hart Hagerty</td>
+              <td>Desktop Support Technician</td>
+              <td>Purple</td>
+            </tr>
+            {/* row 3 */}
+            <tr>
+              <th>3</th>
+              <td>Brice Swyre</td>
+              <td>Tax Accountant</td>
+              <td>Red</td>
+            </tr>
+            <tr>
+              <th>3</th>
+              <td>Brice Swyre</td>
+              <td>Tax Accountant</td>
+              <td>Red</td>
+            </tr>
+            <tr>
+              <th>3</th>
+              <td>Brice Swyre</td>
+              <td>Tax Accountant</td>
+              <td>Red</td>
+            </tr>
+            <tr>
+              <th>3</th>
+              <td>Brice Swyre</td>
+              <td>Tax Accountant</td>
+              <td>Red</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-    </main>
-  )
+    </div>
+  );
 }
